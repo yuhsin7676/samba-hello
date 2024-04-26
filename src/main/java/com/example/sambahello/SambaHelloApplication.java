6d4e04e271348500b2e814283b2afa924da9f1d7
yushin.im <yushin@oaosu.ru>
2024-04-27 02:33:54 +0300

    Initial commit

---
+++ src/main/java/com/example/sambahello/SambaHelloApplication.java
@@ -0,0 +1,13 @@
+package com.example.sambahello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SambaHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SambaHelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sambahello/SambaHelloApplicationTests.java
@@ -0,0 +1,46 @@
+package com.example.sambahello;
+
+import jcifs.CIFSContext;
+import jcifs.CIFSException;
+import jcifs.CloseableIterator;
+import jcifs.Configuration;
+import jcifs.config.PropertyConfiguration;
+import jcifs.context.BaseContext;
+import jcifs.smb.NtlmPasswordAuthenticator;
+import jcifs.smb.SmbFile;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Properties;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class SambaHelloApplicationTests {
+
+    @Test
+    void start() throws MalformedURLException, CIFSException, IOException {
+        String domain = "192.168.1.103";
+        Properties prop = new Properties();
+        prop.put( "jcifs.smb.client.useExtendedSecurity","false" );
+        prop.put( "jcifs.smb.client.useSMB2Negotiation", "true" );
+        Configuration config = new PropertyConfiguration(prop);
+        CIFSContext baseContext = new BaseContext(config);
+
+
+        CIFSContext contextWithCred = baseContext.withCredentials(new NtlmPasswordAuthenticator(null, "", ""));
+        SmbFile share = new SmbFile("smb://192.168.1.103/public", contextWithCred);
+        boolean a = share.exists();
+
+        CloseableIterator c = share.children();
+
+        int b = 0;
+    }
+}
